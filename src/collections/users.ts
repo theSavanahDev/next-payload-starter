d2bf4887866e41a3ea0732a9ed4c52f8c986862d
Michael Caesar Owuor <michael.owuor@gmail.com>
2024-06-21 15:54:01 +0300

    refactor: update users collection

--- src/collections/users.ts
+++ src/collections/users.ts
@@ -1,13 +1,14 @@
-import type { CollectionConfig } from 'payload'
+import type { CollectionConfig } from "payload";
 
 export const Users: CollectionConfig = {
-  slug: 'users',
-  admin: {
-    useAsTitle: 'email',
-  },
-  auth: true,
-  fields: [
-    // Email added by default
-    // Add more fields as needed
-  ],
-}
+	slug: "users",
+	labels: {
+		singular: "User",
+		plural: "Users",
+	},
+	admin: {
+		useAsTitle: "email",
+	},
+	auth: true,
+	fields: [],
+};
